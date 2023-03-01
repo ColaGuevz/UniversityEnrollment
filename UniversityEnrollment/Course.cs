700cdacdfdc8ce3a4b4324f38c2a5251d149864e
LonelyByStander14 <mrrcguevarra@gmail.com>
2023-03-01 16:30:32 +0800

    Add project files.

---
+++ UniversityEnrollment/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityEnrollment
+{
+    public class Course
+    {
+        private string CourseId { get; set; }
+        private string CourseDescription { get; set; }
+
+        public Course(string courseId, string courseDescription)
+        {
+            this.CourseId = courseId;
+            this.CourseDescription = courseDescription;
+        }
+    }
+}
---
+++ UniversityEnrollment/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityEnrollment
+{
+    internal class Student
+    {
+        private string FirstName {  get; set; }
+        private string MiddleName { get; set; }
+        private string LastName { get; set; }
+        private string Email { get; set; }
+        public string IDnumber { get;private set; }
+        private string EmailAddress { get; set; }
+        private string Address { get; set; }
+        public string Program { get;private set; }
+
+        public Student(string firstName, string middleName, string lastName, string email, string iDnumber, string emailAddress, string address, string program)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.IDnumber = iDnumber;
+            this.EmailAddress = emailAddress;
+            this.Address = address;
+            this.Program = program;
+        }
+    }
+}
